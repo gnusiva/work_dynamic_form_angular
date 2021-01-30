25d975346312155e367066883b2469b5a545b0a6
Siva <sivam1@netapp.com>
2021-01-30 19:43:27 +0530

    moved dynamic component to its own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatRadioModule} from '@angular/material/radio';
+import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DynamicFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MatSelectModule,
-    MatCheckboxModule,
-    MatRadioModule
+    DynamicFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DynamicFormComponent } from './dynamic-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
+
+
+@NgModule({
+  declarations: [DynamicFormComponent],
+  exports: [DynamicFormComponent],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule
+  ]
+})
+export class DynamicFormModule { }
