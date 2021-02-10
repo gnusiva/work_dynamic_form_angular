4021ad7a6e4707a3a9ae905befbf1faaf32d36c0
Siva <gnusiva@gmail.com>
2021-02-10 16:10:02 +0530

    green color for non required field done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,6 +67,8 @@ export class AppComponent {
   dymanicFormInput: DynamicFormInput[] = [
     { displayLabel: 'First Name', fieldType: 'text', value: 'asdf', required: true, validationLabel: 'Please enter name' },
 
+    { displayLabel: 'First Name', fieldType: 'text', value: '', required: false, validationLabel: 'Please enter name' },
+
     { displayLabel: 'select one option', fieldType: 'checkbox', value: false, required: true, validationLabel: 'Please select checkbox' },
 
     { displayLabel: 'Enter Email', fieldType: 'email', value: '', required: true, validationLabel: 'Please enter email',
@@ -79,6 +81,8 @@ export class AppComponent {
       options: [{id: '1', label: 'option1'}, {id: '2', label: 'option2'}, {id: '3', label: 'option3'}] },
 
     { displayLabel: 'select a date', fieldType: 'date', value: '', required: true, validationLabel: 'Please select date.' },
+
+    { displayLabel: 'select a date', fieldType: 'date', value: '', required: false, validationLabel: 'Please select date.' },
 
     { displayLabel: 'Enter a number', fieldType: 'number', value: '', required: true, validationLabel: 'Please select date.' },
 
